da59db934b91bd85e7f68c2900d3f6cf9b2eb4a4
Daniel Kreitsch <hello@danielkreitsch.com>
2023-11-18 21:15:36 +0100

    chore(prettier): update config and reformat workspace

--- jest.config.lib.ts
+++ jest.config.lib.ts
@@ -1,14 +1,14 @@
 /* eslint-disable */
 export default {
-  displayName: 'nx-nakama',
-  preset: './jest.preset.js',
+  displayName: "nx-nakama",
+  preset: "./jest.preset.js",
   transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+    "^.+\\.[tj]s$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.spec.json" }],
   },
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: './coverage/nx-nakama',
+  moduleFileExtensions: ["ts", "js", "html"],
+  coverageDirectory: "./coverage/nx-nakama",
   testMatch: [
-    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
-    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
+    "<rootDir>/src/**/__tests__/**/*.[jt]s?(x)",
+    "<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)",
   ],
-};
+}
